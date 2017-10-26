a70e6d7c9a391e9a57af36d1031353c4428df86d
davidfyffe@yahoo.co.uk <davidfyffe@yahoo.co.uk>
2017-10-26 15:23:52 +0100

    adding speak code

--- skeletonman/playSoundFile.py
+++ skeletonman/playSoundFile.py
@@ -1,4 +1,9 @@
 import os
 
 def play_sound():
-    os.system('aplay /home/pi/skeletonMan/skeletonman/sound.wav')
+    #os.system('aplay /home/pi/skeletonMan/skeletonman/sound.wav')
+    os.system('say hello')
+
+def speak_shit(text):
+    command = "say " + text
+    os.system(command)
--- skeletonman/views.py
+++ skeletonman/views.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 from django.views.generic.base import TemplateView
 from rest_framework.parsers import JSONParser
-import servoControlleri2c as servoController
+# import servoControlleri2c as servoController
 import playSoundFile as playSoundFile
 import thread
-
-# Create your views here.
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 class HomePageView(TemplateView):
-
     template_name = "skeletonIndex.html"
 
     def get_context_data(self, **kwargs):
@@ -37,8 +33,9 @@ def servo_number(request, pk):
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
-        thread.start_new_thread(servoController.move_servo, (pk,))
+        # thread.start_new_thread(servoController.move_servo, (pk,))
         return JsonResponse("some shit from skeletonman servo rest api " + pk, safe=False)
+
 
 @csrf_exempt
 def servo_all(request):
@@ -46,8 +43,8 @@ def servo_all(request):
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
-
         return JsonResponse("some shit from skeletonman servo rest api", safe=False)
+
 
 @csrf_exempt
 def scarysounds(request):
@@ -58,12 +55,15 @@ def scarysounds(request):
         playSoundFile.play_sound()
         return JsonResponse("some shit from skeletonman scary sounds", safe=False)
 
+
 @csrf_exempt
 def speak(request):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'POST':
+
         data = JSONParser().parse(request)
-        print data
-        return JsonResponse("some shit from skeletonman speak some text" + request.data, safe=False)
+        text = data['text']
+        playSoundFile.speak_shit(text)
+        return JsonResponse("some shit from skeletonman speak some text" + text, safe=False)
